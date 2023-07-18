33176d62b35132f4d1e9fa6a94b9dfa619c8e6cb
Catmaster <109808650+catmasterX@users.noreply.github.com>
2023-07-18 12:03:19 -0500

    Add files via upload

---
+++ AntiBloatware.ps1
@@ -0,0 +1,441 @@
+write "Bienvenido a CatOsAntiBloatware para windows"
+$ErrorActionPreference = 'silentlycontinue'
+
+$Button = [Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [Windows.MessageBoxImage]::Error
+$Ask = 'Quieres usar esta aplicacion como Administrador?
+
+        Elige "Yes" Para abrir en modo admnistrador
+
+        Elige "No" (No recomendable) para abrir como usuario normal
+        
+        Elige "Cancel" para detener el script.'
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    $Prompt = [Windows.MessageBox]::Show($Ask, "Run as an Administrator or not?", $Button, $ErrorIco) 
+    Switch ($Prompt) {
+        Yes {
+            Write-Host "No puedes correr este script como un usuario normal. Este script Necesita Permisos de Administrador."
+            Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+            Exit
+        }
+        No {
+            Break
+        }
+    }
+}
+
+$global:Bloatware = @(
+    "Microsoft.BingNews"
+    "Microsoft.GetHelp"
+    "Microsoft.Getstarted"
+    "Microsoft.Messaging"
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.News"
+    "Microsoft.Office.Lens"
+    "Microsoft.Office.OneNote"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.People"
+    "Microsoft.Print3D"
+    "Microsoft.RemoteDesktop"
+    "Microsoft.SkypeApp"
+    "Microsoft.StorePurchaseApp"
+    "Microsoft.Office.Todo.List"
+    "Microsoft.Whiteboard"
+    "Microsoft.WindowsAlarms"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "EclipseManager"
+    "ActiproSoftwareLLC"
+    "AdobeSystemsIncorporated.AdobePhotoshopExpress"
+    "Duolingo-LearnLanguagesforFree"
+    "PandoraMediaInc"
+    "CandyCrush"
+    "Wunderlist"
+    "Flipboard"
+    "Twitter"
+    "Facebook"
+    "Spotify"
+    "Minecraft"
+    "Royal Revolt"
+    "Sway"
+    "Dolby"
+    "Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe"
+    "Microsoft.Advertising.Xaml_10.1712.5.0_x86__8wekyb3d8bbwe"
+    "Microsoft.BingWeather"
+)
+
+$global:WhiteListedApps = @(
+    "Microsoft.WindowsCalculator"
+    "Microsoft.WindowsStore"
+    "Microsoft.Windows.Photos"
+    "CanonicalGroupLimited.UbuntuonWindows"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.MSPaint"
+    "Microsoft.WindowsCamera"
+    "\.NET"
+    "Microsoft.HEIFImageExtension"
+    "Microsoft.ScreenSketch"
+    "Microsoft.StorePurchaseApp"
+    "Microsoft.VP9VideoExtensions"
+    "Microsoft.WebMediaExtensions"
+    "Microsoft.WebpImageExtension"
+    "Microsoft.DesktopAppInstaller"
+    "WindSynthBerry"
+    "MIDIBerry"
+    "Slack"
+)
+
+$NonRemovables = Get-AppxPackage -AllUsers | Where-Object { $_.NonRemovable -eq $true } | foreach { $_.Name }
+$NonRemovables += Get-AppxPackage | Where-Object { $_.NonRemovable -eq $true } | foreach { $_.Name }
+$NonRemovables += Get-AppxProvisionedPackage -Online | Where-Object { $_.NonRemovable -eq $true } | foreach { $_.DisplayName }
+$NonRemovables = $NonRemovables | Sort-Object -unique
+
+if ($NonRemovables -eq $null ) {
+    $NonRemovables = @(
+        "1527c705-839a-4832-9118-54d4Bd6a0c89"
+        "c5e2524a-ea46-4f67-841f-6a9465d9d515"
+        "E2A4F912-2574-4A75-9BB0-0D023378592B"
+        "F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE"
+        "InputApp"
+        "Microsoft.AAD.BrokerPlugin"
+        "Microsoft.AccountsControl"
+        "Microsoft.BioEnrollment"
+        "Microsoft.CredDialogHost"
+        "Microsoft.ECApp"
+        "Microsoft.LockApp"
+        "Microsoft.MicrosoftEdgeDevToolsClient"
+        "Microsoft.MicrosoftEdge"
+        "Microsoft.PPIProjection"
+        "Microsoft.Win32WebViewHost"
+        "Microsoft.Windows.Apprep.ChxApp"
+        "Microsoft.Windows.AssignedAccessLockApp"
+        "Microsoft.Windows.CapturePicker"
+        "Microsoft.Windows.CloudExperienceHost"
+        "Microsoft.Windows.ContentDeliveryManager"
+        "Microsoft.Windows.Cortana"
+        "Microsoft.Windows.HolographicFirstRun"
+        "Microsoft.Windows.NarratorQuickStart"
+        "Microsoft.Windows.OOBENetworkCaptivePortal"
+        "Microsoft.Windows.OOBENetworkConnectionFlow"
+        "Microsoft.Windows.ParentalControls"
+        "Microsoft.Windows.PeopleExperienceHost"
+        "Microsoft.Windows.PinningConfirmationDialog"
+        "Microsoft.Windows.SecHealthUI"
+        "Microsoft.Windows.SecondaryTileExperience"
+        "Microsoft.Windows.SecureAssessmentBrowser"
+        "Microsoft.Windows.ShellExperienceHost"
+        "Microsoft.Windows.XGpuEjectDialog"
+        "Microsoft.XboxGameCallableUI"
+        "Windows.CBSPreview"
+        "windows.immersivecontrolpanel"
+        "Windows.PrintDialog"
+        "Microsoft.VCLibs.140.00"
+        "Microsoft.Services.Store.Engagement"
+        "Microsoft.UI.Xaml.2.0"
+    )
+}
+
+Function dotInclude() {
+    Param(
+        [Parameter(Mandatory)]
+        [string]$includeFile
+    )
+    $scriptPath = $PSScriptRoot
+    if ( $PSScriptRoot -eq $null -and $psISE) {
+        $scriptPath = (Split-Path -Path $psISE.CurrentFile.FullPath)
+    }
+    if ( test-path $scriptPath\$includeFile ) {
+        . $scriptPath\$includeFile
+    }
+}
+
+dotInclude 'custom-lists.ps1'
+
+$global:BloatwareRegex = $global:Bloatware -join '|'
+$global:WhiteListedAppsRegex = $global:WhiteListedApps -join '|'
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form = New-Object system.Windows.Forms.Form
+$Form.ClientSize = '800,500'
+$Form.text = "CatOsAntiBloatware"
+$Form.TopMost = $false
+
+$Debloat = New-Object system.Windows.Forms.Label
+$Debloat.text = "Eliga una opcion:"
+$Debloat.AutoSize = $true
+$Debloat.width = 25
+$Debloat.height = 10
+$Debloat.Font = 'Microsoft Sans Serif,12,style=Bold,Underline'
+
+$RemoveAllBloatware = New-Object system.Windows.Forms.Button
+$RemoveAllBloatware.text = "Ganar un iphone 13 pro max"
+$RemoveAllBloatware.width = 142
+$RemoveAllBloatware.height = 40
+$RemoveAllBloatware.location = New-Object System.Drawing.Point(8, 79)
+$RemoveAllBloatware.Font = 'Microsoft Sans Serif,10'
+
+$Form.controls.AddRange(@($Debloat, $CustomizeBlacklists, $RemoveAllBloatware, $RemoveBlacklist, $Label1, $RevertChange, $Label2, $DisableCortana, $EnableCortana, $StopEdgePDFTakeover, $EnableEdgePDFTakeover, $DisableTelemetry, $RemoveRegkeys, $UnpinStartMenuTiles, $RemoveOnedrive, $FixWhitelist, $RemoveBloatNoBlacklist, $InstallNet35, $EnableDarkMode, $DisableDarkMode))
+
+$DebloatFolder = "C:\Temp\CatOsFusionFolder"
+If (Test-Path $DebloatFolder) {
+    Write-Host "$DebloatFolder exists. Skipping."
+}
+Else {
+    Write-Host "The folder "$DebloatFolder" doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
+    Start-Sleep 1
+    New-Item -Path "$DebloatFolder" -ItemType Directory
+    Write-Host "The folder $DebloatFolder was successfully created."
+}
+
+Start-Transcript -OutputDirectory "$DebloatFolder"
+
+$RemoveAllBloatware.Add_Click( { 
+        $ErrorActionPreference = 'silentlycontinue'
+        
+        Function Begin-SysPrep {
+
+            Write-Host "Starting Sysprep Fixes"
+            Write-Host "Adding Registry key to Disable Windows Store Automatic Updates"
+            $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
+            If (!(Test-Path $registryPath)) {
+                Mkdir $registryPath
+                New-ItemProperty $registryPath AutoDownload -Value 2 
+            }
+            Set-ItemProperty $registryPath AutoDownload -Value 2
+
+            Write-Host "Stopping InstallService"
+            Stop-Service InstallService
+            Write-Host "Setting InstallService Startup to Disabled"
+            Set-Service InstallService -StartupType Disabled
+        }
+        
+        Function CheckDMWService {
+
+            Param([switch]$Debloat)
+  
+            If (Get-Service dmwappushservice | Where-Object { $_.StartType -eq "Disabled" }) {
+                Set-Service dmwappushservice -StartupType Automatic
+            }
+
+            If (Get-Service dmwappushservice | Where-Object { $_.Status -eq "Stopped" }) {
+                Start-Service dmwappushservice
+            } 
+        }
+
+        Function DebloatAll {
+            Get-AppxPackage | Where { !($_.Name -cmatch $global:WhiteListedAppsRegex) -and !($NonRemovables -cmatch $_.Name) } | Remove-AppxPackage
+            Get-AppxProvisionedPackage -Online | Where { !($_.DisplayName -cmatch $global:WhiteListedAppsRegex) -and !($NonRemovables -cmatch $_.DisplayName) } | Remove-AppxProvisionedPackage -Online
+            Get-AppxPackage -AllUsers | Where { !($_.Name -cmatch $global:WhiteListedAppsRegex) -and !($NonRemovables -cmatch $_.Name) } | Remove-AppxPackage
+        }
+  
+        New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+  
+        Function Remove-Keys {         
+          
+            $Keys = @(
+          
+                "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+                "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+                "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.MicrosoftOfficeHub_17.7909.7600.0_x64__8wekyb3d8bbwe"
+                "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+                "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+                "HKCR:\Extensions\ContractId\Windows.BackgroundTasks\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+          
+                "HKCR:\Extensions\ContractId\Windows.File\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+          
+                "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+                "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+                "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+                "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+                "HKCR:\Extensions\ContractId\Windows.Launch\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+          
+                "HKCR:\Extensions\ContractId\Windows.PreInstalledConfigTask\PackageId\Microsoft.MicrosoftOfficeHub_17.7909.7600.0_x64__8wekyb3d8bbwe"
+          
+                "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+                "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy"
+                "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.XboxGameCallableUI_1000.15063.0.0_neutral_neutral_cw5n1h2txyewy"
+                "HKCR:\Extensions\ContractId\Windows.Protocol\PackageId\Microsoft.XboxGameCallableUI_1000.16299.15.0_neutral_neutral_cw5n1h2txyewy"
+             
+                "HKCR:\Extensions\ContractId\Windows.ShareTarget\PackageId\ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+            )
+      
+            ForEach ($Key in $Keys) {
+                Write-Host "Removing $Key from registry"
+                Remove-Item $Key -Recurse
+            }
+        }
+          
+        Function Protect-Privacy { 
+  
+            New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+          
+            Write-Host "Disabling Windows Feedback Experience program"
+            $Advertising = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo'
+            If (Test-Path $Advertising) {
+                Set-ItemProperty $Advertising Enabled -Value 0
+            }
+          
+            Write-Host "Stopping Cortana from being used as part of your Windows Search Function"
+            $Search = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search'
+            If (Test-Path $Search) {
+                Set-ItemProperty $Search AllowCortana -Value 0
+            }
+          
+            Write-Host "Stopping the Windows Feedback Experience program"
+            $Period1 = 'HKCU:\Software\Microsoft\Siuf'
+            $Period2 = 'HKCU:\Software\Microsoft\Siuf\Rules'
+            $Period3 = 'HKCU:\Software\Microsoft\Siuf\Rules\PeriodInNanoSeconds'
+            If (!(Test-Path $Period3)) { 
+                mkdir $Period1
+                mkdir $Period2
+                mkdir $Period3
+                New-ItemProperty $Period3 PeriodInNanoSeconds -Value 0
+            }
+                 
+            Write-Host "Adding Registry key to prevent bloatware apps from returning"
+            $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+            If (!(Test-Path $registryPath)) {
+                Mkdir $registryPath
+                New-ItemProperty $registryPath DisableWindowsConsumerFeatures -Value 1 
+            }          
+      
+            Write-Host "Setting Mixed Reality Portal value to 0 so that you can uninstall it in Settings"
+            $Holo = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Holographic'    
+            If (Test-Path $Holo) {
+                Set-ItemProperty $Holo FirstRunSucceeded -Value 0
+            }
+      
+            Write-Host "Disabling live tiles"
+            $Live = 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications'    
+            If (!(Test-Path $Live)) {
+                mkdir $Live  
+                New-ItemProperty $Live NoTileApplicationNotification -Value 1
+            }
+      
+            Write-Host "Turning off Data Collection"
+            $DataCollection = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection'    
+            If (Test-Path $DataCollection) {
+                Set-ItemProperty $DataCollection AllowTelemetry -Value 0
+            }
+      
+            Write-Host "Disabling People icon on Taskbar"
+            $People = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People'
+            If (Test-Path $People) {
+                Set-ItemProperty $People PeopleBand -Value 0
+            }
+  
+            Write-Host "Disabling suggestions on the Start Menu"
+            $Suggestions = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager'    
+            If (Test-Path $Suggestions) {
+                Set-ItemProperty $Suggestions SystemPaneSuggestionsEnabled -Value 0
+            }
+            
+            
+            Write-Host "Removing CloudStore from registry if it exists"
+            $CloudStore = 'HKCUSoftware\Microsoft\Windows\CurrentVersion\CloudStore'
+            If (Test-Path $CloudStore) {
+                Stop-Process Explorer.exe -Force
+                Remove-Item $CloudStore
+                Start-Process Explorer.exe -Wait
+            }
+  
+            reg load HKU\Default_User C:\Users\Default\NTUSER.DAT
+            Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Value 0
+            Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEnabled -Value 0
+            Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name OemPreInstalledAppsEnabled -Value 0
+            reg unload HKU\Default_User
+      
+            Write-Host "Disabling scheduled tasks"
+            Get-ScheduledTask -TaskName XblGameSaveTask | Disable-ScheduledTask
+            Get-ScheduledTask -TaskName Consolidator | Disable-ScheduledTask
+            Get-ScheduledTask -TaskName UsbCeip | Disable-ScheduledTask
+            Get-ScheduledTask -TaskName DmClient | Disable-ScheduledTask
+            Get-ScheduledTask -TaskName DmClientOnScenarioDownload | Disable-ScheduledTask
+        }
+
+        Function UnpinStart {
+            Write-Host "Unpinning all tiles from the start menu"
+            (New-Object -Com Shell.Application).
+            NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').
+            Items() |
+            % { $_.Verbs() } |
+            ? { $_.Name -match 'Un.*pin from Start' } |
+            % { $_.DoIt() }
+        }
+
+        Function Remove3dObjects {
+            Write-Output "Removing 3D Objects from explorer 'My Computer' submenu"
+            $Objects32 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}"
+            $Objects64 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}"
+            If (Test-Path $Objects32) {
+                Remove-Item $Objects32 -Recurse 
+            }
+            If (Test-Path $Objects64) {
+                Remove-Item $Objects64 -Recurse 
+            }
+        }
+
+  
+        Function CheckDMWService {
+
+            Param([switch]$Debloat)
+  
+            If (Get-Service dmwappushservice | Where-Object { $_.StartType -eq "Disabled" }) {
+                Set-Service dmwappushservice -StartupType Automatic
+            }
+
+            If (Get-Service dmwappushservice | Where-Object { $_.Status -eq "Stopped" }) {
+                Start-Service dmwappushservice
+            } 
+        }
+        
+        Function CheckInstallService {
+  
+            If (Get-Service InstallService | Where-Object { $_.Status -eq "Stopped" }) {  
+                Start-Service InstallService
+                Set-Service InstallService -StartupType Automatic 
+            }
+        }
+  
+        Write-Host "Initiating Sysprep"
+        Begin-SysPrep
+        Write-Host "Removing bloatware apps."
+        DebloatAll
+        Write-Host "Removing leftover bloatware registry keys."
+        Remove-Keys
+        Write-Host "Checking to see if any Whitelisted Apps were removed, and if so re-adding them."
+        FixWhitelistedApps
+        Write-Host "Stopping telemetry, disabling unneccessary scheduled tasks, and preventing bloatware from returning."
+        Protect-Privacy
+        Write-Host "Unpinning tiles from the Start Menu."
+        UnpinStart
+        Write-Host "Setting the 'InstallService' Windows service back to 'Started' and the Startup Type 'Automatic'."
+        CheckDMWService
+        CheckInstallService
+        Write-Host "Sistema limpio. Por Favor Salga de la interfaz grafica y reinicie `n"
+  
+    } )
+[void]$Form.ShowDialog()
